0bf4a0f8310333b2ff86f472c549f3736e3af8d9
eduar <eduar@EDUARDO>
2025-11-03 18:22:42 -0400

    tweaking for menu styling improvement

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,6 +7,13 @@ export default function HomePage() {
         <div className="background_image">
           <img src="/images/personal_photo_1000x500px.jpg" alt="Personal Photo" height="100%" />
         </div>
+        <div className="pitch_container">
+          <div className="pitch_text">
+
+          <h1>Eduardo Osteicoechea</h1>
+          <p>I'm a Full Stack - Desktop / Web - Developer and BIM Architect - and UI / UX designer, with a strong focus on the AEC industry and AI Integrations.</p>
+          </div>
+        </div>
       </div>
   )
 }
