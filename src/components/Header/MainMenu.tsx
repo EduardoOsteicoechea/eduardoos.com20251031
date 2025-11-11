01a07027ce1a3d0459286fba5fe34d92ace4c798
eduar <eduar@EDUARDO>
2025-11-11 15:59:42 -0400

    tweaking for menu styling improvement

--- src/components/Header/MainMenu.tsx
+++ src/components/Header/MainMenu.tsx
@@ -8,6 +8,13 @@ interface MainMenuProps {
 }
 
 export default function MainMenu(props: MainMenuProps) {
+
+  const [selectedItem, setSelectedItem] = useState("Home");
+
+  const handleSelected = (selected: string) => {
+    setSelectedItem(selected);
+  }
+
   return (
     <div className={`main_menu ${props.active ? "main_menu_active" : ""}`}>
       <div className="main_menu_link_container">
@@ -16,12 +23,16 @@ export default function MainMenu(props: MainMenuProps) {
           heading="Home"
           message="Meet Eduardo Osteicoechea and discover the website."
           image="/images/icon_home.png"
+          selectedItem={selectedItem}
+          handleSelectedItem={handleSelected}
         />
         <MainMenuItem
           to="/series"
           heading="Series"
           message="Read my articles in ethics and biblical teaching and reflexion."
           image="/images/icon_article.png"
+          selectedItem={selectedItem}
+          handleSelectedItem={handleSelected}
         />
       </div>
     </div>
@@ -33,17 +44,11 @@ interface MainMenuItemProps {
   heading: string,
   message: string,
   image: string,
-
+  selectedItem: string,
+  handleSelectedItem: (a: any) => void,
 }
 
 function MainMenuItem(props: MainMenuItemProps) {
-
-  const [isSelected, setIsSelected] = useState(false);
-
-  const handleClick = () => {
-    setIsSelected(prev => !prev);
-  }
-
   return (
     <div className="main_menu_item">
       <div className="main_menu_item_icon_container">
@@ -52,11 +57,13 @@ function MainMenuItem(props: MainMenuItemProps) {
       <div className="main_menu_item_data_card">
         <div className="main_menu_item_data_card_heading_indicator_container">
           <h3 className="main_menu_item_data_card_heading">{props.heading}</h3>
-          <div className={`main_menu_item_status_indicator ${isSelected ? "selected" : ""}`}></div>
+          <div className={`main_menu_item_status_indicator_outer_ring`}>
+            <div className={`main_menu_item_status_indicator ${props.selectedItem == props.heading ? "selected" : ""}`}></div>
+          </div>
         </div>
         <div className="main_menu_item_data_card_message_button_container">
           <p className="main_menu_item_data_card_message">{props.message}</p>
-          <Link to={props.to} className="main_menu_item_data_card_button" onClick={handleClick}>View</Link>
+          <Link to={props.to} className="main_menu_item_data_card_button" onClick={()=>props.handleSelectedItem(props.heading)}>View</Link>
         </div>
       </div>
     </div>
